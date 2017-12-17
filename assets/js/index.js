cdb441404429bccf3b260376b99ebff6339ec757
clarityzdd <davidrg095@gmail.com>
2017-12-17 01:42:19 +0000

    fichas actores, merchandising y css

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,25 @@ $(document).ready(function(){
         $('.nav li').removeClass("active");
         $(this).addClass("active");
     });
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "todos")
+        {
+            $('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
 });
