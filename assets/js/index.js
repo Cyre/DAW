5eb97b3e377a5958dd9fa8ef75d9c1fcfea0901a
Cyre <kaai.cyre@gmail.com>
2017-12-17 21:22:35 +0000

    Añadido página de contacto y generador de noticias por php

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,8 +40,21 @@ $(document).ready(function(){
 
     ias.extension(new IASSpinnerExtension());
     ias.extension(new IASTriggerExtension({offset: 2}));
-    ias.extension(new IASNoneLeftExtension({text: "You reached the end"}));
+    ias.extension(new IASNoneLeftExtension({text: "No hay más noticias nuevas."}));
 
+    /* Email validation */
+
+    function checkEmail() {
+
+        var email = document.getElementById('correo');
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        if (!filter.test(email.value)) {
+            alert('Please provide a valid email address');
+            email.focus;
+            return false;
+        }
+    }
 });
 
 
---
+++ generador_noticias.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaai
+ * Date: 17/12/2017
+ * Time: 14:39
+ */
+$number = $_GET["count"];
+$noticias = file_get_contents('./assets/datos/noticias.json', true);
+$datos = json_decode($noticias, true);
+
+showFourNews($number,$datos);
+
+
+function showFourNews($start,$datos) {
+        $count = count($datos);
+        if ($start < 0 || $start > $count) {return false;}
+        $end = $start + 4;
+        echo "<div class=\"post\">
+            <div class=\"row\">";
+        for ($i = $start; $i < $end; $i++) {
+            if ($i == $count) {break;}
+            if ($i == $start) {
+                echo "<div class=\"col-md-2 col-md-offset-2\">";
+            } else {
+                echo "<div class=\"col-md-2\">";
+            }
+            $noticia = $datos[$i];
+
+
+            switch ($noticia["display"]) {
+                case "imagen":
+                    muestra_imagen($noticia["contenido"],$noticia["enlace"]);
+                    break;
+                case "titular":
+                    muestra_titular($noticia["contenido"],$noticia["enlace"]);
+                    break;
+                case "cita":
+                    muestra_cita($noticia["contenido"],$noticia["enlace"]);
+                    break;
+            }
+
+        }
+    echo "</div></div></body></html>";
+}
+
+function muestra_cita($contenido,$enlace) {
+    $cita = $contenido["cita"];
+    $autor = $contenido["autor"];
+    echo "<a href=\"$enlace\">
+        <blockquote>
+            <p>$cita</p>
+            <footer>$autor</footer>
+        </blockquote>
+    </a>
+</div>";
+}
+
+function muestra_imagen($contenido,$enlace) {
+    echo "<a href=\"$enlace\"><img src=\"$contenido\"></a></div>";
+}
+
+function muestra_titular($contenido,$enlace) {
+    echo "<a href=\"$enlace\"><p>$contenido</p></a></div>";
+}
+
+?>
